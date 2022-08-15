6a4d4b8170d56c44ce58c0cbe02ac53947e6ab9c
Hoang Trung <hoangtrung1801.2003@gmail.com>
2022-08-15 23:06:29 +0700

    perf: ⚡️ make simple settings

--- next.config.js
+++ next.config.js
@@ -1,34 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  eslint: {
-    dirs: ['src'],
-  },
+    eslint: {
+        dirs: ["src"],
+    },
 
-  reactStrictMode: true,
+    reactStrictMode: true,
 
-  // Uncoment to add domain whitelist
-  // images: {
-  //   domains: [
-  //     'res.cloudinary.com',
-  //   ],
-  // },
-
-  // SVGR
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: [
-        {
-          loader: '@svgr/webpack',
-          options: {
-            typescript: true,
-            icon: true,
-          },
-        },
-      ],
-    });
-
-    return config;
-  },
+    // Uncoment to add domain whitelist
+    // images: {
+    //   domains: [
+    //     'res.cloudinary.com',
+    //   ],
+    // },
 };
